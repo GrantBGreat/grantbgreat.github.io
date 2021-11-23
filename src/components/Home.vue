4ac429e156e554c1b9aecea15c4a493b3fc710b1
GrantBGreat <grant@rbgn.ca>
2021-11-23 14:48:57 -0500

    hehe

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,3 +4,11 @@
     <p>^ that was written 8 months ago lol</p>
   </div>
 </template>
+
+<script>
+  window.addEventListener("load", function(){
+    console.clear();
+    console.error('%c This Website Sucks', 'font-size: 50px; text-transform: uppercase; color: white; text-shadow: 2px 2px red, 4px 4px orange, 6px 6px yellow, 8px 8px green; 10px 10px blue, 12px 12px purple;');
+    console.log('actually tho I only made it because I was bored but I hate frontend so it is very lousy');
+  });
+</script>
