b733db4c5b0af3994016beccc90add1409f24895
GrantBGreat <grant@rbgn.ca>
2021-11-23 15:08:03 -0500

    shh

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,11 @@
   <div>
     <h1>soon™</h1>
     <p>^ that was written 8 months ago lol</p>
+
+    <div class="absolute bottom-0 right-0">
+      <p class="text-xs italic font-thin text-right">shh don't check console</p>
+    </div>
+    
   </div>
 </template>
 
@@ -9,6 +14,6 @@
   window.addEventListener("load", function(){
     console.clear();
     console.error('%c This Website Sucks', 'font-size: 50px; text-transform: uppercase; color: white; text-shadow: 2px 2px red, 4px 4px orange, 6px 6px yellow, 8px 8px green; 10px 10px blue, 12px 12px purple;');
-    console.log('actually tho I only made it because I was bored but I hate frontend so it is very lousy');
+    console.log('Actually tho I only made it because I was bored but I hate frontend so it is very lousy');
   });
 </script>
