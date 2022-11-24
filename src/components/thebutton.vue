7fea73ef14528677ccf38a73e9aaffadb592ad7d
Grant Bourne <grant@rbgn.ca>
2022-11-24 13:30:40 -0500

    fixed button

--- src/components/thebutton.vue
+++ src/components/thebutton.vue
@@ -1,3 +1,3 @@
 <template>
-    <button>The button</button>
+    <div class="grid h-screen place-items-center"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">The button</button></div>
 </template>
