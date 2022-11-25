d55828713f20914d899d5891b9ef7446bc5f3594
Grant Bourne <grant@rbgn.ca>
2022-11-25 13:45:33 -0500

    oh no don't rick roll me

--- src/components/thebutton.vue
+++ src/components/thebutton.vue
@@ -1,3 +1,3 @@
 <template>
-    <div class="grid h-screen place-items-center"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">The button</button></div>
+    <div class="grid h-screen place-items-center"><a href="https://youtu.be/dQw4w9WgXcQ"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">The button</button></a></div>
 </template>
